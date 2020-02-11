bf4d261829f0a0e0cfafb85ab53fc73843cc09b0
Junology <jtachan@hotmail.com>
2020-02-11 12:06:15 +0900

    Fix typo in test_math.cpp.

--- test/test_math.cpp
+++ test/test_math.cpp
@@ -6,14 +6,14 @@ TEST(MathMisc, TriangleIntersection2D)
 {
     std::array<Eigen::Vector2d,3> reftrig {
         Eigen::Vector2d(0.0, 0.0),
-        Eigen::Vector2d(1,0, 0.0),
+        Eigen::Vector2d(1.0, 0.0),
         Eigen::Vector2d(0.0, 1.0)
     };
 
     EXPECT_TRUE(
-        triangleIntersection2D(
+        trianglesIntersection2D(
             reftrig,
             { Eigen::Vector2d(0.5, 0.5),
               Eigen::Vector2d(-0.5, 0.5),
-              Eigen::Vector2d(0.5, -0.5) });
+              Eigen::Vector2d(0.5, -0.5) }));
 }
